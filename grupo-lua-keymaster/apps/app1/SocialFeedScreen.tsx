e382666349eaaf7c63c4a98f778682be2e91770b
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-06 15:11:23 -0300

    App1: ativa Social V5 com status e perfil

--- grupo-lua-keymaster/apps/app1/SocialFeedScreen.tsx
+++ grupo-lua-keymaster/apps/app1/SocialFeedScreen.tsx
@@ -1,26 +1,35 @@
 import type { App1Role } from "./api";
 import { SocialArchive } from "./SocialArchive";
-import { SocialFeedScreenV4 } from "./SocialFeedScreenV4";
+import { SocialFeedScreenV5 } from "./SocialFeedScreenV5";
 
 export function SocialFeedScreen({
   sessionToken,
   deviceToken,
+  viewerProfileId,
+  viewerPublicName,
   viewerRole = "ADM",
+  sessionExpiresAt,
   onOpenChat = () => {},
   onOpenProfile = () => {}
 }: {
   sessionToken: string;
   deviceToken: string;
+  viewerProfileId: string | null;
+  viewerPublicName: string;
   viewerRole?: App1Role;
+  sessionExpiresAt: string;
   onOpenChat?: () => void;
   onOpenProfile?: () => void;
 }) {
   return (
     <>
-      <SocialFeedScreenV4
+      <SocialFeedScreenV5
         sessionToken={sessionToken}
         deviceToken={deviceToken}
+        viewerProfileId={viewerProfileId}
+        viewerPublicName={viewerPublicName}
         viewerRole={viewerRole}
+        sessionExpiresAt={sessionExpiresAt}
         onOpenChat={onOpenChat}
         onOpenProfile={onOpenProfile}
       />
