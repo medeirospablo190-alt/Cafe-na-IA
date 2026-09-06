d4d5869d136e651b306b7d150e1e81e41ab0a119
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-06 12:53:11 -0300

    App1: ativa Social V4

--- grupo-lua-keymaster/apps/app1/SocialFeedScreen.tsx
+++ grupo-lua-keymaster/apps/app1/SocialFeedScreen.tsx
@@ -1,22 +1,28 @@
 import type { App1Role } from "./api";
 import { SocialArchive } from "./SocialArchive";
-import { SocialFeedScreen as SocialFeedScreenV3 } from "./SocialFeedScreenV3";
+import { SocialFeedScreenV4 } from "./SocialFeedScreenV4";
 
 export function SocialFeedScreen({
   sessionToken,
   deviceToken,
-  viewerRole = "ADM"
+  viewerRole = "ADM",
+  onOpenChat = () => {},
+  onOpenProfile = () => {}
 }: {
   sessionToken: string;
   deviceToken: string;
   viewerRole?: App1Role;
+  onOpenChat?: () => void;
+  onOpenProfile?: () => void;
 }) {
   return (
     <>
-      <SocialFeedScreenV3
+      <SocialFeedScreenV4
         sessionToken={sessionToken}
         deviceToken={deviceToken}
         viewerRole={viewerRole}
+        onOpenChat={onOpenChat}
+        onOpenProfile={onOpenProfile}
       />
       <SocialArchive sessionToken={sessionToken} deviceToken={deviceToken} />
     </>
