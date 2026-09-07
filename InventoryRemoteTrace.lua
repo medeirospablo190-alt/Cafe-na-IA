befd28e87612c0c7ba29f12181e98329e159b678
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-07 10:17:25 -0300

    Point stable inventory trace loader to V6.1 passive-safe

--- InventoryRemoteTrace.lua
+++ InventoryRemoteTrace.lua
@@ -1,3 +1,3 @@
 -- CAFEINA • INVENTORY REMOTE TRACE
--- Stable loader: current implementation lives in InventoryRemoteTraceV6.lua
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/InventoryRemoteTraceV6.lua"))()
+-- Stable loader: current implementation lives in InventoryRemoteTraceV6_1.lua
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/InventoryRemoteTraceV6_1.lua"))()
