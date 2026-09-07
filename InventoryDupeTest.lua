a84cb45468707dff6006ca31f440aa3053f951ae
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-07 11:42:40 -0300

    Point dupe test loader to race pickup V2.1

--- InventoryDupeTest.lua
+++ InventoryDupeTest.lua
@@ -1,3 +1,3 @@
--- CAFEINA • CRYSTAL DUP CONSISTENCY TEST
--- Stable loader: current implementation lives in InventoryDupeTestV1_1.lua
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/InventoryDupeTestV1_1.lua"))()
+-- CAFEINA • CRYSTAL DUP RACE TEST
+-- Stable loader: current implementation lives in InventoryDupeRaceTestV2_1.lua
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/InventoryDupeRaceTestV2_1.lua"))()
