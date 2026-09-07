b1342e9c7dce5f937040b2dcafccffd9bb6b9c3f
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-07 20:48:44 -0300

    StealAnEgg: aponta loader estável para V6.2

--- StealAnEggClientMenu.lua
+++ StealAnEggClientMenu.lua
@@ -1,3 +1,3 @@
 -- CAFEINA • STEAL AN EGG • CLIENT MENU
--- Stable loader: current implementation lives in StealAnEggClientMenuV6_1.lua
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/StealAnEggClientMenuV6_1.lua"))()
+-- Stable loader: current implementation lives in StealAnEggClientMenuV6_2.lua
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/StealAnEggClientMenuV6_2.lua"))()
