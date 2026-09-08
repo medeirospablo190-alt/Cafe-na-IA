1211b8ffc2d152427a8b0275e513f1760e448a6f
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-07 21:40:11 -0300

    Point AutoEggDelivery loader to V1.2

--- AutoEggDeliveryMenu.lua
+++ AutoEggDeliveryMenu.lua
@@ -1,3 +1,3 @@
 -- CAFEINA • AUTO EGG DELIVERY
--- Stable loader: current implementation lives in AutoEggDeliveryMenuV1_1.lua
-return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/AutoEggDeliveryMenuV1_1.lua"))()
+-- Stable loader: current implementation lives in AutoEggDeliveryMenuV1_2.lua
+return loadstring(game:HttpGet("https://raw.githubusercontent.com/medeirospablo190-alt/Cafe-na-IA/main/AutoEggDeliveryMenuV1_2.lua"))()
