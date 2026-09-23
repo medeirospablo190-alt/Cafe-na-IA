5756afe6c7e741c8fa3b4526ca81515aab9f12dc
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-23 10:56:03 -0300

    build: use Maven-published Filament 1.77.0

--- executor-app/app/build.gradle.kts
+++ executor-app/app/build.gradle.kts
@@ -49,8 +49,8 @@ android {
 
 
 dependencies {
-    implementation("com.google.android.filament:filament-android:1.77.1")
-    implementation("com.google.android.filament:filamat-android:1.77.1")
+    implementation("com.google.android.filament:filament-android:1.77.0")
+    implementation("com.google.android.filament:filamat-android:1.77.0")
 
     testImplementation("junit:junit:4.13.2")
     androidTestImplementation("androidx.test.ext:junit:1.2.1")
