1632293cc21eef48d1b28bd71867a59b01922c2f
medeirospablo190-alt <medeirospablo190@gmail.com>
2026-09-23 11:41:33 -0300

    render: add resolved world matrix to render items

--- render-core/include/cafeina/render/RenderScene.hpp
+++ render-core/include/cafeina/render/RenderScene.hpp
@@ -1,5 +1,6 @@
 #pragma once
 
+#include <array>
 #include <vector>
 
 #include "cafeina/world/World.hpp"
@@ -9,13 +10,19 @@ namespace cafeina::render {
 struct RenderItem {
     world::ObjectId objectId = 0;
     world::PrimitiveMesh primitive = world::PrimitiveMesh::Box;
+
+    // Local transform is preserved for inspection/editor tooling.
     world::Transform transform;
+
+    // Column-major world transform for graphics backends.
+    std::array<double, 16> worldMatrix{};
 
     bool operator==(const RenderItem& other) const
     {
         return objectId == other.objectId
             && primitive == other.primitive
-            && transform == other.transform;
+            && transform == other.transform
+            && worldMatrix == other.worldMatrix;
     }
 };
 
